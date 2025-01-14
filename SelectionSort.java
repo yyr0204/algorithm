af3ecd5e8e4fff2d455bd355b0d444c2fe194dd1
yyr0204 <yyr0204@naver.com>
2025-01-14 17:29:23 +0900

    선택정렬 알고리즘

---
+++ SelectionSort.java
@@ -0,0 +1,34 @@
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Input Arr: ");
+        bw.flush();
+        String[] strArr = br.readLine().split(" ");
+
+
+        int [] arr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            arr[i] = Integer.parseInt(strArr[i]);
+        }
+
+        int min;
+        int tmp;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = arr[i];
+            tmp = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    tmp = j;
+                }
+
+                arr[tmp] = arr[i];
+                arr[i] = min;
+
+            }
+        }
+
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
